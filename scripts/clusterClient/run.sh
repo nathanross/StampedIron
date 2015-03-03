b00d1e93126d2e20248b46178fed19b76af654de
Nathan Ross <nross@simplyinteractiveinc.com>
2015-03-03 15:21:16 -0500

    automation improvements

--- scripts/clusterClient/run.sh
+++ scripts/clusterClient/run.sh
@@ -5,7 +5,7 @@ utildir=${DIR}/util/
 #to be run inside a VM where you have ensured internet access
 cp ${DIR}/jessie.sources.list /etc/apt/sources.list
 
-apt-get -y update && apt-get upgrade
+apt-get -y update && apt-get -y upgrade
 apt-get -y install intel-microcode git mercurial curl wget
 cp wlan0.home /etc/network/interfaces.d/wlan0.home
 
