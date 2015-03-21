60046b6c9d12c6fc5936110514402522cece0a58
Nathan Ross <nross@simplyinteractiveinc.com>
2015-03-21 16:20:52 -0400

    smoothing over various bugs, papercuts

--- scripts/clusterClient/run.sh
+++ scripts/clusterClient/run.sh
@@ -6,8 +6,9 @@ utildir=${DIR}/util/
 cp ${DIR}/jessie.sources.list /etc/apt/sources.list
 
 apt-get -y update && apt-get -y upgrade
-apt-get -y install intel-microcode git mercurial curl wget
-cp wlan0.home /etc/network/interfaces.d/wlan0.home
+apt-get -y install intel-microcode git mercurial curl wget ntp
+echo 'snd-hda-intel' >> /etc/modules-load.d/modules.conf
+cp ${dir}/wlan0.home /etc/network/interfaces.d/wlan0.home
 
 $utildir/./ramboot.sh
 btrfs balance start /
