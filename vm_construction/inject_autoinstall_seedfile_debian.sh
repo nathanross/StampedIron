a6d429476722b2f9b0db12708300eb21bedabb4f
Nathan Ross <nross@simplyinteractiveinc.com>
2015-05-07 15:53:24 -0400

    licensing before upload

--- vm_construction/inject_autoinstall_seedfile_debian.sh
+++ vm_construction/inject_autoinstall_seedfile_debian.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Copyright 2015 Nathan Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#!/bin/bash
 # to run:
 # pass as 1st argument the name of the preseed file you want to inject.
 # optional 2nd argument: path to 'cp -r' to cd root
