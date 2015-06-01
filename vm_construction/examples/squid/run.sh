f3224898f494dd69669216d786788764e646d305
Nathan Ross <nross@simplyinteractiveinc.com>
2015-05-31 23:42:54 -0400

    squid changes

--- vm_construction/examples/squid/run.sh
+++ vm_construction/examples/squid/run.sh
@@ -1,19 +1,19 @@
 echo "
-deb http://ftp.us.debian.org/debian testing main contrib
+deb http://ftp.us.debian.org/debian stable main contrib
 deb http://ftp.debian.org/debian/ jessie-updates main contrib
 deb http://security.debian.org/ jessie/updates main contrib
 " > /etc/apt/sources.list
 
+#give some time for the network to go up
+sleep 10
 apt-get -y update
 apt-get -y install squid3
 
 #~8000MB cache
-echo -e "cache_dir aufs /var/spool/squid3 8000 16 256\n
-maximum_object_size 200MB\n
+echo -e "cache_dir aufs /var/spool/squid3 8000 16 256
+maximum_object_size 200 MB
 " >> /etc/squid3/squid.conf
 service squid3 stop
-killall squid3 
-#squid has bug where service stop sometimes doesn't always end process.
 squid3 -z #create cache directories
 service squid3 start
 
