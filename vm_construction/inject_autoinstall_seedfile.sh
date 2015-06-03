5995a9f1f466f26d36988880a22de6fb59d65ddb
Nathan Ross <nross@simplyinteractiveinc.com>
2015-06-03 02:12:06 -0400

    set IFS no no possible word splitting problems with paths with spaces in them etc. refactor late command shim cmd out, document and cleanup

--- vm_construction/inject_autoinstall_seedfile.sh
+++ vm_construction/inject_autoinstall_seedfile.sh
@@ -22,10 +22,29 @@
 # assumes you want to inject the seedfile in ubunt server,
 # change the appropriate bash glob pattern below if not.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export IFS=''
 
 error() {
     echo -e $@
     exit 1
+}
+
+append_late_command() {
+    local -n ret_string=$1
+    local shellscript=$2
+    local seed_data=$3
+    
+    #ensure each line ends with semicolon and then
+    # replace newlines with spaces
+    shellscript=`echo "$shellscript" | sed -r 's/([^;])$/\1;/g' | tr '\n' ' '`
+    
+    if [ `echo "$seed_data" | grep -E '^d-i preseed/late_command string ' $seedpath | wc -l` -gt 0 ]
+    then
+        ret_string=`echo -e "$seed_data" | sed -r "s@^(d-i preseed/late_command string) @\1 $shellscript @"`
+    else
+        ret_string=`echo -e "$seed_data" "\nd-i preseed/late_command string $shellscript"`
+    fi
+
 }
 
 main() {
@@ -49,60 +68,67 @@ main() {
 "
     fi
 
-    mkdir -p ${d_work}/iso ${d_work}/newiso
+    local d_mntiso=${d_work}/iso
+    local d_newiso=${d_work}/newiso
+    
+    mkdir -p $d_mntiso $d_newiso
     [ $dist = "debian" ] && mkdir -p ${d_work}/irmod
 
     rm -rf ${d_work}/newiso/*
 
-    seedpath=`readlink -f $seedfile`
+    local seedpath=`readlink -f $seedfile`
 
-    if [ `mount | grep -i *.iso | wc -l` -eq 0 ]; then
-        mount -o loop $src_iso ${d_work}/iso
-    fi
+    # if iso is not mounted mount it.
+    grep -qs `basename $src_iso` /proc/mounts || \
+        mount -o loop ${src_iso} ${d_mntiso}
+    
+    # copy over mounted comments to a new directory
+    #-T option is so if newiso dir is already created, copy
+    # original dir over same path instead of subdirectory
+    cp -rT ${d_mntiso} ${d_newiso}
 
-    cp -r ${DIR}/automation_shim ${d_work}/newiso
-    [ $copydir ] && cp -r $copydir ${d_work}/newiso
+    # the automation shim is used to run
+    # on start-up, without any human action, arbitrary run.sh
+    # scripts in specially labeld /dev/sdb partitions.
+    cp -r ${DIR}/automation_shim ${d_newiso}
+    [ $copydir ] && cp -r ${copydir} ${d_newiso}
 
-    cp -rT ${d_work}/iso ${d_work}/newiso
 
-
-    automation_late=`cat ${DIR}/automation_shim/late_command.seed`
-
-    automation_late=`echo "$automation_late" | sed -r 's/([^;])$/\1;/g' | tr '\n' ' '`
-    #ensure each line ends with semicolon and then
-    # replace newlines with spaces
-    seed_data=`cat $seedpath`
-
-    if [ `echo $seed_data | grep -E '^d-i preseed/late_command string ' $seedpath | wc -l` -gt 0 ]
+    if [ $dist = 'debian' ] || [ $dist = 'ubuntu' ]
     then
-        seed_data=`echo "$seed_data" | sed -r "s@^(d-i preseed/late_command string) @\1 $automation_late @"`
-    else
-        seed_data=`echo -e "$seed_data" "\nd-i preseed/late_command string $automation_late"`
+        # adding the automation shim as a startup service
+        # is done by adding a late command on the seedfile
+        # but there can only be one late command, so if a late
+        # command is extant in the user's preseed, the shim
+        # late command is appended to it.
+
+        local shim_script=`cat ${DIR}/automation_shim/late_command.seed`
+        local seed_original=`cat $seedpath`
+        local seed_final='';
+        append_late_command seed_data ${shim_script} ${seed_original}
+
+        #echo "$seed_data"
+
+        if [ $dist = "ubuntu" ]; then
+            echo en > ${d_newiso}/isolinux/lang
+            sed -ri 's/ (file=.cdrom.preseed.ubuntu-server.seed) *vga=[0-9]+/auto=true locale=en_US console-setup\/layoutcode=us \1 /g' "${d_newiso}/isolinux/txt.cfg"
+            echo -e "$seed_data \n" | cat - ${d_newiso}/preseed/ubuntu-server.seed > /tmp/tmpseed
+            sed -ri 's/(steps.*)(language|timezone|keyboard|user|network),//g' /tmp/tmpseed
+            sed -ri 's/timeout +string +[0-9]{1,2}/timeout string 0/g' /tmp/tmpseed
+            cp /tmp/tmpseed ${d_newiso}/preseed/ubuntu-server.seed
+        elif [ $dist = "debian" ]; then
+            sed -ri 's/timeout 0/timeout 1/g' ${d_newiso}/isolinux/isolinux.cfg
+            # don't know which folder actually requires it, should find out through A/B testing.
+            # Which is the only way to be sure as their documentation on this is poor.
+            cd ${d_work}/irmod
+            gzip -d < ${d_newiso}/install.amd/initrd.gz | \
+                cpio --extract --make-directories --no-absolute-filenames
+            echo "$seed_data" > ./preseed.cfg
+            find . | cpio -H newc --create | \
+                gzip -9 > ${d_newiso}/install.amd/initrd.gz
+            cd ../
+        fi
     fi
-
-    echo "$seed_data"
-
-    d_newiso=${d_work}/newiso
-    if [ $dist = "ubuntu" ]; then
-        echo en > ${d_newiso}/isolinux/lang
-        sed -ri 's/ (file=.cdrom.preseed.ubuntu-server.seed) *vga=[0-9]+/auto=true locale=en_US console-setup\/layoutcode=us \1 /g' ${d_newiso}/isolinux/txt.cfg
-        echo -e "$seed_data \n" | cat - ${d_newiso}/preseed/ubuntu-server.seed > /tmp/tmpseed
-        sed -ri 's/(steps.*)(language|timezone|keyboard|user|network),//g' /tmp/tmpseed
-        sed -ri 's/timeout +string +[0-9]{1,2}/timeout string 0/g' /tmp/tmpseed
-        cp /tmp/tmpseed ${d_newiso}/preseed/ubuntu-server.seed
-    elif [ $dist = "debian" ]; then
-        sed -ri 's/timeout 0/timeout 1/g' ${d_newiso}/isolinux/isolinux.cfg
-        # don't know which folder actually requires it, should find out through A/B testing.
-        # Which is the only way to be sure as their documentation on this is poor.
-        cd ${d_work}/irmod
-        gzip -d < ${d_newiso}/install.amd/initrd.gz | \
-            cpio --extract --verbose --make-directories --no-absolute-filenames
-        echo "$seed_data" > ./preseed.cfg
-        find . | cpio -H newc --create --verbose | \
-            gzip -9 > ${d_newiso}/install.amd/initrd.gz
-        cd ../
-    fi
-
     #D stands for disable deep directory relocation
     #r stands for rock ridge directory information
     #V "" stands for the volume ID name.
