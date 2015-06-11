25e7a20c321e2a9860965396c54bb61d4576c712
Nathan Ross <nross@simplyinteractiveinc.com>
2015-06-11 18:16:27 -0400

    allow bootprio to be omitted, providing low priority defaults in L->R order of decreasing priority

--- run_image.sh
+++ run_image.sh
@@ -18,7 +18,31 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export IFS=''
 error() { echo -e $@; exit 1; }
-split(){ local -n ret=$1; IFS=$2; ret=($3); }
+split() {
+    local -n ret=$1
+    local -r delim=$2
+    local -r txt=$3
+    IFS='';
+    ret=();
+    local i=0;
+    local delim_len=${#delim}
+    local agg='' char='' lookahead='' on_delim=1;
+    while [ $i -lt ${#txt} ]; do
+        char="${txt:$i:1}"
+        lookahead="${txt:$i:$delim_len}"
+        if [ $lookahead = $delim ]; then
+            ret+=("$agg")
+            agg=""
+            i=`expr $i + $delim_len`
+            on_delim=1
+        else
+            agg="${agg}${char}"
+            i=`expr $i + 1`
+            on_delim=0
+        fi
+    done
+    [ $on_delim -eq 0 ] && ret+=("$agg")    
+}
 usage() {
     error "\n
 run_image.sh <image>:prio (<folder or .img to add as sdb>:prio)\n
@@ -78,19 +102,18 @@ main() {
     rm -rf /tmp/si_env
     mkdir -p /tmp/si_env
     envdir=/tmp/si_env
-    insert disk_args 1 "$envdir:200" "$@"
+    insert disk_args 1 "$envdir::200" "$@"
     i=0
     for x in ${disk_args[*]}
     do
         unset arr_disk
-        split arr_disk ':' "$x"
-        l_disk=`readlink -f ${arr_disk[0]}`
-        bootprio=${arr_disk[1]}
+        split arr_disk :: "$x"
+        l_disk=`readlink -f ${arr_disk[0]}`        
+        bootprio=${arr_disk[1]:-"`expr 100 + $i`"}
         envadd=${arr_disk[2]}
         [[ $bootprio =~ $int_re ]] || usage
         [ ! -e $l_disk ] && \
             error "asked to use disk at $l_disk but no file/dir exists there"
-        bootprio=${arr_disk[1]}
         if [[ ${l_disk} =~ \.iso$ ]]; then
             device="type='file' device='cdrom'"
             driver="name='qemu' type='raw'"
