bf20cf86f49938b515cca03301d08a6be29f5dde
Nathan Ross <nross.se@gmail.com>
2015-06-25 16:52:46 -0400

    switch to using m4 for preseeds for DRYness

--- examples/squid_image.sh
+++ examples/squid_image.sh
@@ -3,7 +3,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 src=/var/cache/install_discs/debian*.iso
 export OUTDIR=/srv/squid \
        SRC=${src[0]} \
-       SEEDFILE="\$cookbook/debian.ext4.seed" \
+       SEEDFN="m4 \$cookbook/debian.seed.m4" \
        RUN_VARS="WAIT_FOR_IP=1" \
        RECIPES="\$cookbook/squid \$cookbook/shutdown"
 
